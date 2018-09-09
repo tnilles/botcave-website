8b483a7d3faff0f40dd92ce46d5632292620c463
Thierry Nilles <contact@thierrynilles.com>
2018-09-10 01:31:09 +0200

    Add call to action button

---
+++ main.js
@@ -0,0 +1,31 @@
+var modal = new tingle.modal({
+    footer: false,
+    stickyFooter: false,
+    closeMethods: ['overlay', 'button', 'escape'],
+    closeLabel: "Close",
+    cssClass: ['custom-class-1', 'custom-class-2'],
+    onOpen: function() {
+        console.log('modal open');
+    },
+    onClose: function() {
+        console.log('modal closed');
+    },
+    beforeClose: function() {
+        // here's goes some logic
+        // e.g. save content before closing the modal
+        return true; // close the modal
+        return false; // nothing happens
+    }
+});
+
+// set content
+modal.setContent(document.getElementById('modal-content').innerHTML);
+
+// open modal
+//modal.open();
+
+[...document.querySelectorAll('.call-to-action')].forEach(cta => {
+  cta.onclick = () => {
+    modal.open();
+  };
+});
